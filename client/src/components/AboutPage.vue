9e08bf076b96b463f1355882e088be4c7f672214
imkotar <kotarbinski.marcin@gmail.com>
2020-06-14 13:02:54 +0200

    vuex refactor part 4

--- client/src/components/AboutPage.vue
+++ client/src/components/AboutPage.vue
@@ -4,7 +4,7 @@
       <p>About this application</p>
     </v-card-title>
     <v-card-text>
-      Desc
+      {{info}}
     </v-card-text>
   </v-card>
 </template>
@@ -12,7 +12,12 @@
 <script>
 
 export default {
-  name: 'AboutPage'
+  name: 'AboutPage',
+  data () {
+    return {
+      info: 'This is my full-stack CRUD application using: Express, Vue.js, Vuetify, Vuex and MongoDb. As logged user you will have access to create/modify/update posts displayed on the Main Page. You can access the administration panel after registering and logging in. There is already registered account that you can acces by clicking "Test Login" under Login button. The app is constantly upgraded and history of that process can be seen on my GitHub repo.'
+    }
+  }
 }
 </script>
 
